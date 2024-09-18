a961d88295641dd1390d87a53920b2ab56b1cac2
uchar <omid.es73@gmail.com>
2024-09-18 17:17:25 +0330

    Revert prettier on kc.gen

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -10,6 +11,7 @@ export default [
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
+    pluginReact.configs.flat["jsx-runtime"],
     {
         plugins: {
             "react-refresh": reactRefresh
